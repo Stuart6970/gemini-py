c011dbfbc7782a644628b2e619440ba8b7d47330
Stuart Hodge <s.hodge6970@gmail.com>
2024-06-03 12:04:17 +0800

    add config settings

--- gemini-image.py
+++ gemini-image.py
@@ -3,12 +3,43 @@ import PIL.Image
 import google.generativeai as genai
 
 def main():
-    # Get the API key from environment variable
+    # Get the API key from environment variable 
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
     
     img = PIL.Image.open('food.jpg')
 
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    generation_config = {
+        "temperature": 1,
+         "top_p": 0.95,
+        "top_k": 64,
+         "max_output_tokens": 8192,
+        "response_mime_type": "text/plain",
+    }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+    ]
+
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        safety_settings=safety_settings,
+        generation_config=generation_config,
+    )
     response = model.generate_content(["Write a short, engaging blog post based on this picture. It should include a description of the meal in the photo and talk about my journey meal prepping.", img], stream=True)
     response.resolve()
 
